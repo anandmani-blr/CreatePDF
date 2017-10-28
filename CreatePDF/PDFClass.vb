e1f4f89b578452c102dc3b9d6e815d9bb1579190
anandmani-blr <33175156+anandmani-blr@users.noreply.github.com>
2017-10-28 16:50:58 +0530

    Conditional Issue
    Conditional Issue

--- CreatePDF/PDFClass.vb
+++ CreatePDF/PDFClass.vb
@@ -6,22 +6,29 @@ Public Class PDFClass
         'strSourcePath = replace(strSourcePath,"")
         'Anand Added new code to test
         Try
-            If UCase(strKey) = "VS1961" Then
-                fnCreatePDF = False
-                Dim workbook As New Workbook()
-                workbook.LoadFromFile(strSourcePath)
-                strDestPath = strSourcePath
-                strDestPath = Replace(strDestPath, ".xls", ".pdf")
-                strDestPath = Replace(strDestPath, ".xlsx", ".pdf")
-                workbook.SaveToFile(strDestPath, Spire.Xls.FileFormat.PDF)
-                fnCreatePDF = True
+            If LCase(strKey) <> "" Then
+                'Changed to key
+                If UCase(strKey) = "VS1961A" Then
+                    fnCreatePDF = False
+                    Dim workbook As New Workbook()
+                    workbook.LoadFromFile(strSourcePath)
+                    strDestPath = strSourcePath
+                    strDestPath = Replace(strDestPath, ".xls", ".pdf")
+                    strDestPath = Replace(strDestPath, ".xlsx", ".pdf")
+                    workbook.SaveToFile(strDestPath, Spire.Xls.FileFormat.PDF)
+                    fnCreatePDF = True
+                Else
+                    fnCreatePDF = False
+                    strDestPath = "Incorrect Key"
+                End If
             Else
                 fnCreatePDF = False
-                strDestPath = "Incorrect Key"
+                strDestPath = Err.Description
             End If
         Catch EX As Exception
             fnCreatePDF = False
             strDestPath = Err.Description
+
         End Try
     End Function
 End Class
