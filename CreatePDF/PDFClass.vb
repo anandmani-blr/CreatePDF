f1d0329c95491d46236456ad909d7ccd3282148a
anandmani-blr <33175156+anandmani-blr@users.noreply.github.com>
2017-10-28 15:36:12 +0530

    Initial commit

---
+++ CreatePDF/PDFClass.vb
@@ -0,0 +1,26 @@
+﻿Imports Spire.Xls
+Imports Spire.Pdf
+Imports Spire.Xls.Converter
+Public Class PDFClass
+    Public Function fnCreatePDF(ByVal strKey As String, ByVal strSourcePath As String, ByRef strDestPath As String) As Boolean
+        'strSourcePath = replace(strSourcePath,"")
+        Try
+            If UCase(strKey) = "VS1961" Then
+                fnCreatePDF = False
+                Dim workbook As New Workbook()
+                workbook.LoadFromFile(strSourcePath)
+                strDestPath = strSourcePath
+                strDestPath = Replace(strDestPath, ".xls", ".pdf")
+                strDestPath = Replace(strDestPath, ".xlsx", ".pdf")
+                workbook.SaveToFile(strDestPath, Spire.Xls.FileFormat.PDF)
+                fnCreatePDF = True
+            Else
+                fnCreatePDF = False
+                strDestPath = "Incorrect Key"
+            End If
+        Catch EX As Exception
+            fnCreatePDF = False
+            strDestPath = Err.Description
+        End Try
+    End Function
+End Class
